847629c5c51a00b1d61369b87c3dc70998da2d87
vadik184 <133372327+vadik184@users.noreply.github.com>
2024-09-01 21:34:31 +0300

    v1.1.1

--- js/index.js
+++ js/index.js
@@ -1,14 +1,40 @@
 const startContainer = document.querySelector(".start-container");
 const startButton = document.querySelector(".start-button");
-const maleButton = document.querySelector(".male-button");
-const femaleButton = document.querySelector(".female-button");
+// const maleButton = document.querySelector(".male-button");
+// const femaleButton = document.querySelector(".female-button");
 const mainContainer = document.querySelector(".main-container");
+let maleButton;
+let femaleButton;
 
 function openGender() {
   startContainer.style.display = "none";
   startButton.style.display = "none";
-  maleButton.style.display = "block";
-  femaleButton.style.display = "block";
+
+  maleButton = document.createElement("button");
+  maleButton.classList.add("male-button");
+  maleButton.textContent = "хлопець";
+  mainContainer.append(maleButton);
+  maleButton.addEventListener("click", createText);
+
+  femaleButton = document.createElement("button");
+  femaleButton.classList.add("female-button");
+  femaleButton.textContent = "дівчина";
+  mainContainer.append(femaleButton);
+
+  console.log(maleButton, femaleButton);
+
   return maleButton, femaleButton;
+}
+
+function createText() {
+  maleButton.style.display = "none";
+  femaleButton.style.display = "none";
+  const textBoy = document.createElement("p");
+  textBoy.style.width = "100px";
+  textBoy.style.height = "100px";
+  textBoy.style.backgroundColor = "red";
+  textBoy.textContent = "я хлопець";
+  console.log(textBoy);
+  mainContainer.appendChild(textBoy);
 }
 startButton.addEventListener("click", openGender);
